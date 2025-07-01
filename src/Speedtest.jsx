b0a000bb473575fc2977a3b6d9d3c7e6c078b554
Susmita Banda <susmitabanda28@gmail.com>
2025-07-01 14:13:00 +0530

    bg-color chnages

--- src/Speedtest.jsx
+++ src/Speedtest.jsx
@@ -8,7 +8,7 @@ const Speedtest = () => {
   const [wpm, setWpm] = useState(0);
 
   const intervalRef = useRef(null);
-  const sampleText = "The quick brown fox jumps over the lazy dog.";
+  const sampleText = "The old clock chimed, its brass gears groaning with each rotation. Sunlight streamed through the dusty windowpanes, illuminating motes dancing in the air. A forgotten book lay open on the worn armchair, its pages brittle with age. Outside, a gentle breeze rustled the leaves of a lone oak tree, creating a soft, whispering sound. The aroma of brewing coffee drifted from the kitchen, mingling with the faint scent of rain from earlier that morning. A cat stretched languidly on the hearth rug, its emerald eyes half-closed in contentment.";
 
   const handleChange = (e) => {
     const value = e.target.value;
@@ -26,6 +26,7 @@ const Speedtest = () => {
 
     setInput(value);
 
+    
     // If user finishes typing
     if (value === sampleText) {
       clearInterval(intervalRef.current);
@@ -49,8 +50,8 @@ const Speedtest = () => {
 
   return (
     <div style={{ maxWidth: "600px", margin: "auto", padding: "2rem" }}>
-      <h1 className="text-green-800 font-serif text-5xl mb-6">⏱ Typing Speed Test ⌨</h1>
-      <p class="text-2xl mb-3 text-red-500 font-mono"><strong>⌨Type this:</strong></p>
+      <h1 className="text-yellow-100 font-serif text-5xl mb-6">⏱ Typing Speed Test ⌨</h1>
+      <p class="text-2xl mb-3 text-blue-100 font-mono"><strong>⌨ Type this:</strong></p>
       <p style={{ background: "#eee", padding: "1rem", borderRadius: "8px", fontStyle:"italic"}}>
         {sampleText}
       </p>
@@ -65,11 +66,11 @@ const Speedtest = () => {
       />
 
       <div style={{ marginTop: "1rem" }}>
-        <p class="text-2xl mb-3 text-red-500 font-mono"><strong>⏱ Time:</strong> {timeElapsed} seconds</p>
+        <p class="text-2xl mb-3 text-blue-100 font-mono"><strong>⏱ Time:</strong> {timeElapsed} seconds</p>
         {isFinished && <p><strong>WPM:</strong> {wpm}</p>}
         <button onClick={handleReset}
-        style={{backgroundColor:'green',
-                color: 'white',
+        style={{backgroundColor:'yellow',
+                color: 'black',
                 padding:'10px 20px',
                 borderRadius:'10px',
                 fontWeight: 'bold'
